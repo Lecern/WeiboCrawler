fb1d5d2d1304c4e9a1206b794e1bd1292b8cd69e
Jerry <gaozhiwei1993@gmail.com>
2020-01-19 21:44:20 +0900

    init

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 source /etc/profile
+/home/gao/.pyenv/shims/python /home/gao/project/WeiboCrawler/WeiboSearch/login.py >> /home/gao/project/WeiboCrawler/WeiboSearch/log/cookie.log
+sleep 3
 /home/gao/.pyenv/shims/python /home/gao/project/WeiboCrawler/WeiboSearch/run.py >> /home/gao/project/WeiboCrawler/WeiboSearch/log/bash.log
