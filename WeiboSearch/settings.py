88b46516d8a73898778323058c3d93414c61a0bd
Jerry <gaozhiwei1993@gmail.com>
2020-01-19 17:28:46 +0900

    init

--- WeiboSearch/settings.py
+++ WeiboSearch/settings.py
@@ -102,7 +102,7 @@ RETRY_HTTP_CODES = [401, 403, 408, 414, 500, 502, 503, 504]
 
 LOCAL_MONGO_HOST = '127.0.0.1'
 LOCAL_MONGO_PORT = 27017
-DB_NAME = 'weibo_records'
+DB_NAME = 'weibo_pneumonia'
 WEIBO_COLLECTION = 'weibos'
 USER_COLLECTION = 'users'
 
