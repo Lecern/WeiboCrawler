b7b4d865b913f6c6d40031ba0a8245db63dba5fb
Jerry <gaozhiwei1993@gmail.com>
2020-04-11 17:57:39 +0900

    init
    (cherry picked from commit 594947c70373cc37205ca90736d4df6e71f0abd4)
    
    # Conflicts:
    #	WeiboSearch/spiders/weibo_spider.py

--- history.sh
+++ history.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source /etc/profile
-if [[ $# -lt 2 ]]
+if [[ $# -lt 3 ]]
 then
     echo "Missing variable."
     exit 1
@@ -16,4 +16,4 @@ sleep 3
 sleep 3
 /home/gao/.pyenv/shims/python /home/gao/project/WeiboCrawler/WeiboSearch/run.py --keyword="新冠肺炎" --start=${1} --end=${2}>> /home/gao/project/WeiboCrawler/WeiboSearch/log/bash.log
 sleep 3
-/home/gao/.pyenv/shims/python /home/gao/project/WeiboCrawler/WeiboSearch/daily_word_cloud.py --date=${2} >> /home/gao/project/WeiboCrawler/WeiboSearch/log/upload_image.log
+/home/gao/.pyenv/shims/python /home/gao/project/WeiboCrawler/WeiboSearch/daily_word_cloud.py --date=${3} >> /home/gao/project/WeiboCrawler/WeiboSearch/log/upload_image.log
